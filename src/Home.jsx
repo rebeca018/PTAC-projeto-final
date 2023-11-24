854161ab3aa93f935f037b183c8772834665544b
rebeca018 <rebecasnascimento018@gmail.com>
2023-11-24 13:57:30 -0400

    Commit tentando fazer a home

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,9 +1,17 @@
 export default function Home(){
     return(
         
-       <div>
-        <h1>PTAC 2 - PROJETO FINAL</h1>
-       </div>
+        <div class="home">
+        <h3 class="texto-home">Minhas músicas favoritas</h3>
+            <div class="nav">
+                Adicionar nova música
+            </div>
 
+            <div class="video">
+                <div class="card"></div>
+            </div>
+
+        </div>
+       
      );
 }
