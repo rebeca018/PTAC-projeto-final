e908b7593cae7cfe4971b4b33bc12df49ff96548
rebeca018 <rebecasnascimento018@gmail.com>
2023-11-28 08:30:30 -0400

    commit criando rotas

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom/client'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Home from './Home'
 import Cadastro from './cadastro'
+import Destaque from './destaque'
+import Detalhe from './detalhe'
 import './style.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
@@ -11,6 +13,9 @@ ReactDOM.createRoot(document.getElementById('root')).render(
       <Routes>
         <Route path="/" element={<Home />}></Route>
         <Route path="/cadastro" element={<Cadastro />}></Route>
+        <Route  path="/destaque" element={<Destaque />}></Route>
+        <Route  path="/detalhe" element={<Detalhe />}></Route>
+
       </Routes>
     </BrowserRouter>
   </React.StrictMode>,
